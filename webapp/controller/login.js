e53292a2b78a127eaab10381b38ef99dce72b573
yhangang <yhangang@126.com>
2016-03-18 18:57:03 +0800

    加入bootstrapvalidator表单验证功能

--- webapp/controller/login.js
+++ webapp/controller/login.js
@@ -1,8 +1,9 @@
 //载入导航和页脚
 $("#navigation").load("view/navigation.html");
 $("#footer").load("view/footer.html");
+
 // 注册按钮监听事件
-$("#submitButton").click(function() {
+/*$("#submitButton").click(function() {
 	$.post("login.do", $("#loginForm").serialize(), function(data, status) {
 		var obj = JSON.parse(data);
 		if("SUCCESS" == obj.FLAG){
@@ -12,4 +13,47 @@ $("#submitButton").click(function() {
 			$("#loginInfo").attr("class", "alert alert-danger").text("登录失败，请检查用户名与密码");
 		}
 	});
+});*/
+//表单校验
+$(document).ready(function() {
+    $('#loginForm').bootstrapValidator({
+        fields: {
+        	username: {
+                message: 'The username is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 3,
+                        max: 30,
+                        message: 'The username must be more than 3 and less than 30 characters long'
+                    }/*,
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: 'The username can only consist of alphabetical, number and underscore'
+                    }*/
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'The password is required and cannot be empty'
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function(e) {
+        // 修复bootstrapvalidator重复提交的bug
+        e.preventDefault();
+        $.post("login.do", $("#loginForm").serialize(), function(data, status) {
+    		var obj = JSON.parse(data);
+    		if("SUCCESS" == obj.FLAG){
+    			$("#loginInfo").attr("class", "alert alert-info").text("登录成功"); 
+    			setTimeout("location.href = 'mainUrl.do'",1000);
+    		}else{
+    			$("#loginInfo").attr("class", "alert alert-danger").text("登录失败，请检查用户名与密码");
+    		}
+    	});
+    });
 });
--- webapp/controller/register.js
+++ webapp/controller/register.js
@@ -2,7 +2,7 @@
 $("#navigation").load("view/navigation.html");
 $("#footer").load("view/footer.html");
 // 注册按钮监听事件
-$("#registerButton").click(function() {
+/*$("#registerButton").click(function() {
 	$.post("register.do", $("#registerForm").serialize(), function(data, status) {
 		var obj = JSON.parse(data);
 		if("SUCCESS" == obj.FLAG){
@@ -12,4 +12,38 @@ $("#registerButton").click(function() {
 			$("#loginInfo").attr("class", "alert alert-danger").text("注册失败，服务器异常");
 		}
 	});
+});*/
+//表单校验
+$(document).ready(function() {
+    $('#registerForm').bootstrapValidator({
+        fields: {
+        	username: {
+                validators: {
+                    notEmpty: {
+                        message: 'The username name is required and cannot be empty'
+                    }
+                }
+            },
+            realname: {
+                validators: {
+                    notEmpty: {
+                        message: 'The realname address is required'
+                    }
+                }
+            }
+        },
+    }).on('success.form.bv', function(e) {
+        // 修复bootstrapvalidator重复提交的bug
+        e.preventDefault();
+        alert("aa");
+        $.post("register.do", $("#registerForm").serialize(), function(data, status) {
+    		var obj = JSON.parse(data);
+    		if("SUCCESS" == obj.FLAG){
+    			$("#loginInfo").attr("class", "alert alert-info").text("注册成功"); 
+    			setTimeout("location.href = 'mainUrl.do'",1000);
+    		}else{
+    			$("#loginInfo").attr("class", "alert alert-danger").text("注册失败，服务器异常");
+    		}
+    	});
+    });
 });
